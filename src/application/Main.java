95d6a1be060fdc5eb69d88cc955878fdae3341be
afeilke1 <afeilke1@hs-mittweida.de>
2019-11-16 12:40:31 +0100

    Refactored Point and Rectangle, Fixed Main and Game.java

---
+++ src/application/Main.java
@@ -0,0 +1,38 @@
+package application;
+
+import darstellung.MainMenu;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	public static final int WIDTH = 720, HEIGHT = 720;
+	
+	
+	public static Stage primaryStage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			Main.primaryStage = primaryStage;
+			
+			final BorderPane root = new MainMenu();
+			final Scene scene = new Scene(root, WIDTH, HEIGHT);
+			primaryStage.setScene(scene);
+			
+			
+			
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
