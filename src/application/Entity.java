9a3c7c344209c519ebce0be2d7079ee90e145431
afeilke1 <afeilke1@hs-mittweida.de>
2019-11-16 12:42:15 +0100

    refactored Points and Rectangle class, fixed Main.java

--- src/application/Entity.java
+++ src/application/Entity.java
@@ -5,13 +5,13 @@ import javafx.scene.image.Image;
 
 public class Entity extends Rectangle {
 	
-	public int x, y, width, height;
-	
 	public final String src;
 	
 	private final Image img;
 	
 	public Entity(String src, int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
 		this.x = x;
 		this.y = y;
 		
