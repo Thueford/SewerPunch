273c228f3ce22fa5dba480835eb229c63a5ebc0c
lwutke <lwutke@mtm-3913504.hs-mittweida.de>
2019-11-16 14:37:59 +0100

    finished Haribo, I think

--- src/entities/Haribo.java
+++ src/entities/Haribo.java
@@ -5,8 +5,10 @@ public class Haribo extends application.Entity{
 	private static final String src_sound = "";
 	private static final String src_img = "";
 	
+	private static final double speed_init = 2;
+	private static final int HP_init = 1;
 	
-	public Haribo(String src, int x, int y, int width, int height, double speed, int HP) {
-		super(src_img, src_sound, x, y, width, height, speed, HP);
+	public Haribo(String src, int x, int y, int width, int height) {
+		super(src_img, src_sound, x, y, width, height, speed_init, HP_init);
 	}
 }
