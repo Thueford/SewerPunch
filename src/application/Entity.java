f3610e09faa15e0fddc4ec5cb12f98e339593e1d
afeilke1 <afeilke1@hs-mittweida.de>
2019-11-16 15:05:04 +0100

    extended abstract Entity class

--- src/application/Entity.java
+++ src/application/Entity.java
@@ -5,44 +5,54 @@ import Sounds.Sound;
 import Sounds.SoundLoader;
 import javafx.scene.image.Image;
 
-public class Entity extends Rectangle {
+public abstract class Entity extends Rectangle {
 	
 	public final String src_img;
 	public final Image img;
 	public final Sound sound;
+
+	public Point speed;
+	public int HP;
 	
-	private static double speed;
-	private static int HP;
-	
-	public Entity(String src_img, String src_sound, int x, int y, int width, int height, double speed, int HP) {
-		super(x, y, width, height);
+	public Entity(String src_img, String src_sound, String src_dieanim, int x, int y) {
+		super(x, y, 1, 1);
 		
 		this.x = x;
 		this.y = y;
 		
 		this.width = width;
 		this.height = height;
-		this.speed = speed;
-		this.HP = HP;
+		this.speed = getInitSpeed();
+		this.HP = getInitHP();
 		
 		this.src_img = src_img;
 		this.img = Loader.LoadImage(src_img);
 		this.sound = SoundLoader.LoadSound(src_sound);
 	}
+
+	public abstract int getInitHP();
+	public abstract Point getInitSpeed();
+	public abstract Point getInitSize();
 	
 	public static double getSpeed() {
 		return speed;
 	}
 	public static void setSpeed(double speed) {
-		Entity.speed = speed;
+		this.speed = speed;
 	}
 
 	public boolean isDead() {
-		return this.HP<=0;
+		return this.HP <= 0;
 	}
 
 	//Entitäten töten
 	public void die() {
-		
+		onDie();
 	}
+	
+	public abstract void move();
+	public abstract void attack();
+	
+	public abstract void onDie();
+	public abstract void onCollide();
 }
